f005083e8b0992c6aaf775d6e171d35d9a015eac
swdrow <slamsspam@gmail.com>
2026-01-26 22:57:49 +0000

    feat(16-08): create gamification feature barrel export
    - Exports all achievement display components
    - Centralized import point for gamification feature

---
+++ src/v2/features/gamification/index.ts
@@ -0,0 +1,14 @@
+// Components
+export { AchievementBadge } from './components/AchievementBadge';
+export { ProgressBar } from './components/ProgressBar';
+export { AchievementCard } from './components/AchievementCard';
+export { PinnedBadges } from './components/PinnedBadges';
+export { AchievementGrid } from './components/AchievementGrid';
+export { ChallengeCard } from './components/ChallengeCard';
+export { LeaderboardLive } from './components/LeaderboardLive';
+export { PRCelebration } from './components/PRCelebration';
+export { PRSparkline } from './components/PRSparkline';
+export { ShareableCard } from './components/ShareableCard';
+
+// Hooks
+export { useShareCard } from './hooks/useShareCard';
