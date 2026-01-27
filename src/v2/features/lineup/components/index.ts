006ae3851c783d590dc33c4c01b009f7f4c92f5c
swdrow <slamsspam@gmail.com>
2026-01-27 13:23:26 +0000

    feat(18-09): export new lineup components from index
    - Export TemplateManager
    - Export LineupComparison
    - Export RiggingPanel and EquipmentPicker (from previous plans)
    - Group Phase 18 components together
    
    File: src/v2/features/lineup/components/index.ts

--- src/v2/features/lineup/components/index.ts
+++ src/v2/features/lineup/components/index.ts
@@ -4,3 +4,9 @@ export {
   MobileLineupSkeleton,
 } from './LineupSkeleton';
 export { HistoricalLineupBrowser } from './HistoricalLineupBrowser';
+
+// Phase 18 components
+export { RiggingPanel } from './RiggingPanel';
+export { EquipmentPicker } from './EquipmentPicker';
+export { TemplateManager } from './TemplateManager';
+export { LineupComparison } from './LineupComparison';
